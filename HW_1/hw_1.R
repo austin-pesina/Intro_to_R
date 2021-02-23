7d988d76b63e67c1206d6b11120df942d930215b
Austin Pesina <apesina1991@gmail.com>
2021-02-23 14:19:57 -0600

    HW 1-1

--- HW_1/hw_1.R
+++ HW_1/hw_1.R
@@ -4,6 +4,16 @@ setwd("~/Intro_to_R/HW_1")
 NEI <- read_rds ("summarySCC_PM25.rds")
 SCC <- read_rds ("Source_Classification_Code.rds")
 
-years <- filter (NEI, year %in% c(1999, 2002, 2005, 2008))
 
-#ggplot (years) + geom_point (mapping = aes (year, Emissions, fill=type))
+
+#Question 1: Have total emissions from PM2.5 decreased in the US from 99 to 08? Using the base plotting
+#system, make a plot showing the total PM2.5 emission from all sources for each of the years 99, 02, 05, 08.
+
+nei <- aggregate(Emissions ~ year, NEI, sum)
+
+plot(nei$year, nei$Emissions, type = "o", col = "blue",
+     main = "Total PM 2.5 Emission From 1999 to 2008", xlab = "Year", ylab = "Emissions")
+
+
+
+#Question 2: Have total emissions from PM2.5 decreased in San Antonio (fips=="48029") from 99-08?
