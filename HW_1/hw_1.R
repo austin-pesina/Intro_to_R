b02cda5836290e2cd8e49a715b02dd360cf17ad5
Austin Pesina <apesina1991@gmail.com>
2021-02-24 12:24:37 -0600

    hw_1_done

--- HW_1/hw_1.R
+++ HW_1/hw_1.R
@@ -41,16 +41,33 @@ ggplot (type_sa, mapping = aes (year, Emissions, col = type)) +
 #Question 4: Across the United States, how have emissions from coal combustion-related sources changed
 #from 99-08?
 
-ggplot (SCC)
+scc_coal <- SCC[grepl("coal", SCC$Short.Name, ignore.case = T),]
+nei_coal <- NEI[NEI$SCC %in% scccoal$SCC,]
+coal_tot <- aggregate(Emissions ~ year + type, neicoal, sum)
+
+ggplot (coal_tot, aes(year, Emissions, col = type)) +
+  ggtitle (expression("Emissions from Coal Combustion-Related Sources")) +
+  geom_point() +
+  geom_line()
 
 #Question 5: How have emissions from  motor vehicle sources changed from 99-08 in SA?
-cars <- filter(type_sa, type == "ON-ROAD")
-mv_sa <- aggregate(Emissions ~ year + cars, sa, sum)
+cars <- filter(NEI, NEI$fips =="48029" & NEI$type == "ON-ROAD")
+mv_sa <- aggregate(Emissions ~ year, cars, sum)
 
-#ggplot (type_sa, mapping = aes (year, Emissions, col = type)) + 
-  ggtitle (expression("Emissions by Type in San Antonio (1999-2008)")) +
+ggplot (mv_sa, mapping = aes (year, Emissions)) + 
+  ggtitle (expression("Motor Vehicle Emissions in San Antonio (1999-2008)")) +
   geom_point () + 
-  geom_line()
+  geom_line(col = "blue")
 
 #Question 6: Compare emissions from motor vehicle sources in SA with emissions from motor vehicle sources
 #in LA County (fips=="06037"). Which city has seen greater changes over time in motor vehicle emissions?
+
+
+sa_la <- filter(NEI, NEI$fips %in% c("06037", "48209") & NEI$type == "ON-ROAD")
+mv_sa_la <- aggregate (Emissions ~ year + fips, sa_la, sum)
+
+ggplot (mv_sa_la, mapping = aes (year, Emissions, col = fips)) + 
+  ggtitle (expression("Motor Vehicle Emissions: Los Angeles vs San Antonio (1999-2008)")) +
+  geom_point () + 
+  geom_line() +
+  scale_color_discrete(name = "City", labels = c("Los Angeles", "San Antonio"))
